c04b9ff1b807361d1c06e8741744691282d3ca9d
Vitor Palhano <vitorpalhano2014@gmail.com>
2021-05-05 00:07:54 -0300

    Finalização do cadastro de Vaga

---
+++ EstacionaAki/src/Controlles/VagaController.java
@@ -0,0 +1,21 @@
+package Controlles;
+
+import Models.VagaEstacionamento;
+import java.util.ArrayList;
+
+public class VagaController {
+	
+	private static ArrayList<VagaEstacionamento> estacionamento = new ArrayList<VagaEstacionamento>();
+	
+	
+	
+	public static boolean cadastrar(VagaEstacionamento vagaNova) {
+		for(VagaEstacionamento vagaExiste : estacionamento) {
+			if(vagaExiste.getLocal().equals(vagaNova.getLocal())) {
+				return false;
+			}
+		}
+		estacionamento.add(vagaNova);
+		return true;
+	}
+}
--- EstacionaAki/src/Views/VagaCadastrar.java
+++ EstacionaAki/src/Views/VagaCadastrar.java
@@ -1,6 +1,8 @@
 package Views;
 
 import java.util.Scanner;
+
+import Controlles.VagaController;
 import Models.VagaEstacionamento;
 
 public class VagaCadastrar {
@@ -8,12 +10,18 @@ public class VagaCadastrar {
 	private static VagaEstacionamento vaga;
 	
 	public static void cadastroVaga() {
-		System.out.println("\n".repeat(15));
+		//System.out.println("\n".repeat(15));
 		vaga = new VagaEstacionamento();
 		System.out.println("\n\t###CADASTRO DE VAGA###\n");
 		System.out.println("Informe a localização da vaga:");
 		vaga.setLocal(sc.nextLine());
 		System.out.println(vaga);
+		if(VagaController.cadastrar(vaga)) {
+			System.out.println("Vaga Cadastratada!");
+		} else {
+			System.out.println("Já existe vaga para este local!");
+		}
+		
 	}
 	
 	
