04a2904226d970c737efbd7870582af651d66f4f
Vitor Palhano <vitorpalhano2014@gmail.com>
2021-06-24 19:54:51 -0300

    ..

--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListarComponent } from './components/listar/listar.component';
+import { CadastrarComponent } from './components/cadastrar/cadastrar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListarComponent,
+    CadastrarComponent
   ],
   imports: [
     BrowserModule,
