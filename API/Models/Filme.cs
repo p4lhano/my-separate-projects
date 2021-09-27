131f196e06f166cfb57891db7f62ad5d61a226af
Vitor Palhano <72114099+p4lhano@users.noreply.github.com>
2021-09-27 20:00:52 -0300

    Inicial e Back

---
+++ API/Models/Filme.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace API.Models
+{
+    public class Filme
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public int Nota { get; set; }
+        public DateTime CriadoEm { get; set; }
+            public Filme() => CriadoEm = DateTime.Now;
+    }
+}
