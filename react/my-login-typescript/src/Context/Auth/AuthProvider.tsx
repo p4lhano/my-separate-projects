ffaf028b8388577a449573420f3da2474abf7789
Vitor Palhano <vitorpalhano2014@gmail.com>
2023-02-02 22:57:59 -0300

    Ajuste de login em REACT

--- react/my-login-typescript/src/Context/Auth/AuthProvider.tsx
+++ react/my-login-typescript/src/Context/Auth/AuthProvider.tsx
@@ -9,30 +9,28 @@ export const AuthProvider = ({children}: {children: JSX.Element}) => {
     const api = useApi();
 
     const validadeToken = async () => {
+        
         const storageDataToken = localStorage.getItem('authToken');
         if (storageDataToken){
+            
             const data = await api.validadeToken(storageDataToken);
-            if (data.user) {
+            if (data.user) 
                 setUser(data.user);
-            }
+            
         }
     }
-    useEffect( () => { // esta gerando loopp infinito esse use effect e uso da api
+    useEffect( () => { 
         validadeToken();
-    },[]); // a ausencia de dependencia esta gerando apenas o useEffect quando deveria
+    },[]);
     
 
     const signin = async (email: string, password: string) => {
-        console.log('Esta usando signin');
 
         const data = await api.singin(email, password);
-        console.log('Logou api.singin(email, password');
         if (data.user && data.token){
-            console.log(`setando ${data.user}`);
-            console.log(`setando ${data.token}`);
+            
             setUser(data.user);
             setToken(data.token);
-            console.log('Terminou');
             
             return true;
         }
@@ -40,7 +38,7 @@ export const AuthProvider = ({children}: {children: JSX.Element}) => {
 
     }
     const singout = async () => {
-        console.log('Esta usando sigout');
+        
         await api.singout();
         setUser(null);
         setToken('');
--- react/my-login-typescript/src/hooks/useApi.ts
+++ react/my-login-typescript/src/hooks/useApi.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosResponse } from 'axios';
+import axios from 'axios';
 
 const api = axios.create({
     baseURL: process.env.REACT_APP_API
@@ -17,24 +17,24 @@ export const useApi = () => ({
     singin: async (email: string, password: string) => {
 
         // provisório para testes
-        // return {
-        //     user: {id: 3, name: 'Jose', email: 'jose@jose.com'},
-        //     token: '123abc'
-        // };
-        try{
-            console.log('Testando logn');
+        return {
+            user: {id: 3, name: 'Jose', email: 'jose@jose.com'},
+            token: '123abc'
+        };
+        // try{
+        //     console.log('Testando logn');
             
-            let response: AxiosResponse<any,any> ; // falta realizar tratativas para erros, como 401
-            response = await api.post('/auth/login',{email,password})
-            //.then(resp => response = resp)
-            //.catch(error => console.log(error));
-            console.log('Chegou da api resposta');
-            console.log(response);
-            return response?.data ;
-        }catch(e){
-            console.log(e);
+        //     let response: AxiosResponse<any,any> ; // falta realizar tratativas para erros, como 401
+        //     response = await api.post('/auth/login',{email,password})
+        //     //.then(resp => response = resp)
+        //     //.catch(error => console.log(error));
+        //     console.log('Chegou da api resposta');
+        //     console.log(response);
+        //     return response?.data ;
+        // }catch(e){
+        //     console.log(e);
             
-        }
+        // }
     },
     singout:async () => {
         // const responser = await api.post('/singout');
