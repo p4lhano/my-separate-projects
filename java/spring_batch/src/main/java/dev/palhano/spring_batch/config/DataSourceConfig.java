c52a4597dbc967b187d960369fa8ab20acb8174f
Vitor Palhano <vitorpalhano2014@gmail.com>
2023-08-01 22:51:02 -0300

    [add] configuracao de log via log4j2

--- java/spring_batch/src/main/java/dev/palhano/spring_batch/config/DataSourceConfig.java
+++ java/spring_batch/src/main/java/dev/palhano/spring_batch/config/DataSourceConfig.java
@@ -10,15 +10,15 @@ import javax.sql.DataSource;
 
 @Configuration
 public class DataSourceConfig {
-    @Primary
-    @Bean
-    @ConfigurationProperties(prefix = "spring.datasource")
-    public DataSource springDataSource() {
-        return DataSourceBuilder.create().build();
-    }
-    @Bean
-    @ConfigurationProperties(prefix = "app.datasource")
-    public DataSource appDataSource() {
-        return DataSourceBuilder.create().build();
-    }
+//    @Primary
+//    @Bean
+//    @ConfigurationProperties(prefix = "spring.datasource")
+//    public DataSource springDataSource() {
+//        return DataSourceBuilder.create().build();
+//    }
+//    @Bean
+//    @ConfigurationProperties(prefix = "app.datasource")
+//    public DataSource appDataSource() {
+//        return DataSourceBuilder.create().build();
+//    }
 }
