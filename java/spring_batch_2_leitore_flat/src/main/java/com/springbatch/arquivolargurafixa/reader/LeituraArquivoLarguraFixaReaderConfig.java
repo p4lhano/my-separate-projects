f53a554da57338b23ba99163235b3b2943ab7979
Vitor Palhano <vitorpalhano2014@gmail.com>
2023-08-05 21:01:42 -0300

    feat(spring_batch): leitura de arquivos com delimitador

--- java/spring_batch_2_leitore_flat/src/main/java/com/springbatch/arquivolargurafixa/reader/LeituraArquivoLarguraFixaReaderConfig.java
+++ java/spring_batch_2_leitore_flat/src/main/java/com/springbatch/arquivolargurafixa/reader/LeituraArquivoLarguraFixaReaderConfig.java
@@ -13,16 +13,28 @@ import org.springframework.core.io.Resource;
 @Configuration
 public class LeituraArquivoLarguraFixaReaderConfig {
 
+//	@Bean @StepScope
+//	public FlatFileItemReader<Cliente> leituraArquivoLarguraFixaReader(
+//			@Value("#{jobParameters['arquivoClientes']}") Resource arquivoClientes ) {
+//		// leitor de arquivo de largura fixa.
+//		return new FlatFileItemReaderBuilder<Cliente>()
+//				.name("leituraArquivoLarguraFixaReader")
+//				.resource(arquivoClientes)
+//				.fixedLength()
+//				.columns(new Range[]{new Range(1,10),new Range(11,20),new Range(21,23),new Range(24,37)})
+//				.names("nome","sobrenome","idade","email")
+//				.targetType(Cliente.class)
+//				.build();
+//	}
 	@Bean @StepScope
-	public FlatFileItemReader<Cliente> leituraArquivoLarguraFixaReader(
+	public FlatFileItemReader<Cliente> leituraArquivoDelimitadoReader(
 			@Value("#{jobParameters['arquivoClientes']}") Resource arquivoClientes ) {
-		// leitor de arquivo de largura fixa.
+		// leitor de arquivo de com delimitador ','.
 		return new FlatFileItemReaderBuilder<Cliente>()
-				.name("leituraArquivoLarguraFixaReader")
+				.name("leituraArquivoDelimitadoReader")
 				.resource(arquivoClientes)
-				.fixedLength()
-				.columns(new Range[]{new Range(1,10),new Range(11,20),new Range(21,23),new Range(24,43)})
-				.names(new String[]{"nome","sobrenome","idade","email"})
+				.delimited()
+				.names("nome","sobrenome","idade","email")
 				.targetType(Cliente.class)
 				.build();
 	}
